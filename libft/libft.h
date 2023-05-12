d599c5d4d07c57ba859574fa8db33db13daace50
cwenz <cwenz@student.42heilbronn.de>
2023-05-12 15:21:08 +0200

    added libft section header file

---
+++ libft/libft.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cwenz <cwenz@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/15 09:54:15 by cwenz             #+#    #+#             */
+/*   Updated: 2023/05/12 14:35:07 by cwenz            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stddef.h>
+
+# include "check/libft_check.h"
+# include "conversion/libft_conversion.h"
+# include "linked-lists/libft_linked_lists.h"
+# include "mem/libft_mem.h"
+# include "string/libft_string.h"
+# include "write/libft_write.h"
+
+#endif /* LIBFT_H */
