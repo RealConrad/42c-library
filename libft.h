691c60ea7aba0e750e446c98c0f635eb5cd5fd77
cwenz <wenzconrad@gmail.com>
2023-07-10 15:55:15 +0200

    renamed header file

--- libft.h
+++ libft.h
@@ -1,20 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft_main.h                                       :+:      :+:    :+:   */
+/*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cwenz <cwenz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/12 10:30:33 by cwenz             #+#    #+#             */
-/*   Updated: 2023/05/12 18:27:17 by cwenz            ###   ########.fr       */
+/*   Updated: 2023/07/10 15:54:38 by cwenz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_MAIN_H
-# define LIBFT_MAIN_H
+#ifndef LIBFT_H
+# define LIBFT_H
 
 # include "gnl/get_next_line.h"
-# include "libft/libft.h"
+# include "libft/c-library.h"
 # include "printf/ft_printf.h"
 
 #endif /* LIBFT_MAIN_H */
