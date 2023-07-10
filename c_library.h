83e401a7c4c4cb8b1fb8868b10188232dc887c65
cwenz <wenzconrad@gmail.com>
2023-07-10 16:04:30 +0200

    asd

--- c_library.h
+++ c_library.h
@@ -6,15 +6,15 @@
 /*   By: cwenz <cwenz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/12 10:30:33 by cwenz             #+#    #+#             */
-/*   Updated: 2023/07/10 15:57:27 by cwenz            ###   ########.fr       */
+/*   Updated: 2023/07/10 16:03:47 by cwenz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#ifndef C_LIBRARY
+# define C_LIBRARY
 
 # include "gnl/get_next_line.h"
 # include "libft/libft.h"
 # include "printf/ft_printf.h"
 
-#endif /* LIBFT_MAIN_H */
+#endif /* C_LIBRARY */
