e27e1458e548ea6398bd0497edcc7278295c3af2
cwenz <wenzconrad@gmail.com>
2023-07-10 15:58:51 +0200

    changed header file name

--- c_library.h
+++ c_library.h
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   c_library.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cwenz <cwenz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/12 10:30:33 by cwenz             #+#    #+#             */
-/*   Updated: 2023/07/10 15:54:38 by cwenz            ###   ########.fr       */
+/*   Updated: 2023/07/10 15:57:27 by cwenz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 # define LIBFT_H
 
 # include "gnl/get_next_line.h"
-# include "libft/c-library.h"
+# include "libft/libft.h"
 # include "printf/ft_printf.h"
 
 #endif /* LIBFT_MAIN_H */
